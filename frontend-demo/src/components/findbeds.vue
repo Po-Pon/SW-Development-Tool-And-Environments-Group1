204e5a36d1888923f3c8cecc55ff36b92571ea46
Vichayuth <56262507+Vichayuth@users.noreply.github.com>
2022-02-15 17:00:53 +0700

    feat: drop-down และปุ่มค้นหา

--- frontend-demo/src/components/findbeds.vue
+++ frontend-demo/src/components/findbeds.vue
@@ -1,13 +1,71 @@
 <template>
-  <div></div>
+  <br /><br /><br />
+  <div>
+    <!-- FindBeds Section -->
+    <h3 class="my-3"><i class="fas fa-search"></i> ค้นหาเตียง</h3>
+    <div class="row g-2">
+      <div class="col-lg-4 col-3"></div>
+      <div class="col-lg-4 col-6">
+        <select class="form-select" v-model="province">
+          <option
+            v-for="(province, index) in allProvinceTH"
+            :key="index"
+            :value="province"
+          >
+            {{ province }}
+          </option>
+        </select>
+      </div>
+      <div class="col-lg-4 col-3">
+        <button class="btn btn-info" @click="findBedsbyProvince()">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
+    </div>
+
+    <br />
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import { provinceTH } from "../assets/js/province.js";
+import { SERVER_IP, PORT } from "../assets/server/serverIP";
 export default {
   data() {
-    return {};
+    return {
+      allProvinceTH: [],
+      province: "",
+      beds: [],
+      bedsReady: [],
+      amountBedsReady: 0,
+    };
   },
   methods: {
+    findBedsbyProvince() {
+      if (this.province == "ทุกจังหวัด") {
+        this.beds = this.bedsReady;
+      } else {
+        this.beds = this.bedsReady.filter(
+          (array) => array.province == this.province
+        );
+      }
+    },
+    getBedsReady() {
+      axios
+        .get(`https://${SERVER_IP}:${PORT}/bedsready`)
+        .then((res) => {
+          const data = res.data;
+          this.amountBedsReady = data.info.reduce(function (prev, curr) {
+            return prev + curr.amount;
+          }, 0);
+          this.bedsReady = data.info;
+          this.findBedsbyProvince();
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
     authentication() {
       let info = JSON.parse(localStorage.getItem("info"));
       if (info != null) {
@@ -20,6 +78,9 @@ export default {
   },
   created() {
     this.authentication();
+    this.allProvinceTH = ["ทุกจังหวัด", ...provinceTH];
+    this.province = this.allProvinceTH[0];
+    this.getBedsReady();
   },
 };
 </script>
