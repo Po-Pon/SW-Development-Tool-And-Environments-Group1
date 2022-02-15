af267ed30683178d105ed1fc2d334499ba4f5b09
Akira <54892411+docxed@users.noreply.github.com>
2022-02-15 15:45:52 +0700

    feat: เพิ่ม router และ authentication หน้า index, login, register

---
+++ frontend-demo/src/components/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    authentication() {
+      let info = JSON.parse(localStorage.getItem("info"));
+      if (info != null) {
+        this.$root.info = info;
+        this.$root.loggedIn = true;
+        this.$router.push("/");
+      } else {
+        this.loggedIn = false;
+      }
+    },
+  },
+  created() {
+    this.authentication();
+  },
+};
+</script>
+
+<style scoped></style>
