407c8faf8bc200b16aa5189ee1b3e17bfd499a62
phalat080 <phalat18@gmail.com>
2022-02-15 22:15:30 +0700

    feat: สร้าง API ข้อมูลสถานที่ให้บริการเตียงที่ user ได้ทำการจองเรียบร้อยแล้ว

--- backend-demo/routes/bedsdealing.js
+++ backend-demo/routes/bedsdealing.js
@@ -53,4 +53,43 @@ router.post("/bedsdealing", async (req, res) => {
     }
   });
 
+  router.get("/bedsdealing/:id", async (req, res) => {
+    try {
+      const dealingall4 = await Bedsdealing.find({ _id: req.params.id });
+      const bedsid = await Beds.find();
+      const userid = await User.find();
+  
+      let list = [];
+      let namell4;
+      let bedsinfo;
+      for (let y = 0; y < userid.length; y++) {
+        if (dealingall4[0].user_id == userid[y]._id + "") {
+          namell4 = userid[y];
+        }
+      }
+      for (let z = 0; z < bedsid.length; z++) {
+        if (dealingall4[0].bed_id == bedsid[z]._id + "") {
+          bedsinfo = bedsid[z];
+        }
+      }
+  
+      list.push({
+        _id: dealingall4[0]._id,
+        bed_id: dealingall4[0].bed_id,
+        user_id: dealingall4[0].user_id,
+        date:  dealingall4[0].date,
+        user: namell4,
+        bed: bedsinfo,
+      });
+  
+      if (list.length === 0) {
+        res.status(203).json({ status: false, message: "ไม่มีข้อมูล!" });
+      } else {
+        res
+          .status(200)
+          .json({ status: true, message: "การค้นหาสำเร็จ!", info: list });
+      }
+    } catch (err) {}
+  });
+
 module.exports = router;
