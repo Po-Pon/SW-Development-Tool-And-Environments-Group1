52b194ee4fa59f16fb2da2c785dec2e717c3cec0
Akira <54892411+docxed@users.noreply.github.com>
2022-02-15 16:02:03 +0700

    feat: เพิ่ม authentication หน้า findbeds

--- frontend-demo/src/components/findbeds.vue
+++ frontend-demo/src/components/findbeds.vue
@@ -5,9 +5,22 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      authentication() {
+        let info = JSON.parse(localStorage.getItem("info"));
+        if (info != null) {
+          this.$root.info = info;
+          this.$root.loggedIn = true;
+        } else {
+          this.loggedIn = false;
+        }
+      },
+    };
   },
   methods: {},
+  created() {
+    this.authentication();
+  },
 };
 </script>
 
