581376dca7eb75f45fae0ac13fba3dea35d6fba4
prathannw <prathan.dewza123@gmail.com>
2022-02-19 21:47:33 +0700

    feat : สร้าง API สำหรับแก้ไขข้อมูล User

--- backend-demo/routes/user.js
+++ backend-demo/routes/user.js
@@ -21,4 +21,24 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    res
+      .status(200)
+      .json({ status: true, message: "อัพเดทข้อมูลสำเร็จ", info: updatedUser });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "อัพเดทข้อมูลไม่สำเร็จ กรุณาลองใหม่ภายหลัง" });
+  }
+});
+
 module.exports = router;
