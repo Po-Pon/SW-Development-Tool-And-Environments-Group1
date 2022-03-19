3ec031b70ccd8555b80a7c7251af98c2b16d23c3
phalat080 <phalat18@gmail.com>
2022-03-20 04:12:38 +0700

    fix: ทำcontroller มาใช้ในroute

---
+++ backend-demo-test/test/db/user.spec.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+const mongoose = require("mongoose");
+
+describe("users", () => {
+    before(() => mongoose.connect(process.env.MONGO_URL));
+
+    it('find all users is')
+
+})
