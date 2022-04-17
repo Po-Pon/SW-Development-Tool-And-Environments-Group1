8db951e20b1568f241d2e1264bb83c320349c58e
Akira <54892411+docxed@users.noreply.github.com>
2022-04-18 00:15:52 +0700

    fix: test component

--- frontend/tests/unit/component.spec.js
+++ frontend/tests/unit/component.spec.js
@@ -1,26 +1,13 @@
-import { mount } from "@vue/test-utils"
+import { mount, shallowMount } from "@vue/test-utils"
 import Signin from "@/views/Signin.vue"
-import Signup from "@/views/Signup.vue"
+import Findbeds from "@/views/Findbeds.vue"
 import Home from "@/views/Home.vue"
 import Chartdaily from "@/components/Chartdaily.vue"
-import Findbeds from "@/views/Findbeds.vue"
 import Chartstats from "@/components/Chartstats.vue"
-import Provincecovid from "@/components/Provincecovid.vue"
 import Bedsmanage from "@/views/Bedsmanage.vue"
 import Bededit from "@/views/Bededit.vue"
-import Addbedsforsell from "@/views/Addbedsforsell.vue"
 import Bed from "@/views/Bed.vue"
-
-const mockuser = {
-  email: "bizon23868@gmail.com",
-  firstname: "พิชญะ",
-  id: 6,
-  idcard: "1100703167062",
-  lastname: "สิงห์มีศรี",
-  lineid: "bossfuck",
-  phone: "0632659596",
-  role: "user",
-}
+import axios from "axios"
 
 describe("test that signin page have all elements that must be have.", () => {
   const forSignin = mount(Signin)
@@ -44,126 +31,6 @@ describe("test that signin page have all elements that must be have.", () => {
     expect(forSignin.find("button").element.type).toBe("submit")
     expect(forSignin.find("button").element.innerHTML).toBe(
       " ลงชื่อเข้าเข้าใช้งาน "
-    )
-  })
-})
-
-describe("test that Home page have all elements that must be have.", () => {
-  it("test that Home page have all Button for watch Covid - 19 statistic", async () => {
-    const forHome = await mount(Home, {
-      data() {
-        return {
-          covidData: {
-            txn_date: "2022-04-16",
-            new_case: 18892,
-            total_case: 4012184,
-            new_case_excludeabroad: 18810,
-            total_case_excludeabroad: 3988566,
-            new_death: 125,
-            total_death: 26754,
-            new_recovered: 22220,
-            total_recovered: 3763978,
-            update_date: "2022-04-16 07:32:52",
-          },
-          stage: "daily",
-        }
-      },
-    })
-    await expect(forHome.findAll("span").at(0).element.innerHTML).toBe(
-      "ประจำวัน"
-    )
-    await expect(forHome.findAll("span").at(2).element.innerHTML).toBe(
-      "กราฟสถิติข้อมูล"
-    )
-    await expect(forHome.findAll("span").at(3).element.innerHTML).toBe(
-      "รายงานประจำจังหวัด"
-    )
-  })
-})
-
-describe("test that all Covid - 19 statistic components have all elements that must be have.", () => {
-  it("test that Chartdaily component have daily graph of Covid - 19 statistic", async () => {
-    const dailyChart = await mount(Chartdaily)
-    await expect(dailyChart.find("canvas").find("#myChart").exists()).toBe(true)
-  })
-  it("test that Chartstats component have all static graph of Covid - 19 statistic", async () => {
-    const allStatChart = await mount(Chartstats)
-    await expect(allStatChart.find("#myChart").exists()).toBe(true)
-    await expect(allStatChart.find("#myChart2").exists()).toBe(true)
-    await expect(allStatChart.find("#myChart3").exists()).toBe(true)
-  })
-})
-
-describe("test that Beds Manage page have all elements that must be have.", () => {
-  const bedsManage = mount(Bedsmanage, {
-    data() {
-      return {
-        beds: [
-          {
-            address:
-              "108 ซอย โยธา 1 แขวง ตลาดน้อย เขตสัมพันธวงศ์ กรุงเทพมหานคร 10100",
-            amount: 299,
-            customer_amount: 1,
-            id: 50,
-            lat: "13.7321154",
-            lng: "100.5142296",
-            state: "true",
-            timestamp: "2022-04-10T08:14:12.000Z",
-          },
-        ],
-      }
-    },
-  })
-  it("test that Beds Manage page have details blogs for beds places.", async () => {
-    await expect(bedsManage.find("table").exists()).toBe(true)
-  })
-  it("test that Beds Manage page have edit bed page link button.", async () => {
-    await expect(bedsManage.findAll("button").at(2).element.innerHTML).toBe(
-      "แก้ไข"
-    )
-  })
-  it("test that Beds Manage page have add bed button for add bed.", async () => {
-    await expect(bedsManage.findAll("button").at(0).element.innerHTML).toBe(
-      "เพิ่มสถานที่"
-    )
-  })
-})
-
-describe("test that bed info edit page have all elements that must be have.", () => {
-  const editPage = mount(Bededit, {
-    global: {
-      mocks: {
-        $route: {
-          params: {
-            id: 50,
-          },
-        },
-      },
-    },
-    data() {
-      return {
-        bed: {
-          id: 50,
-          amount: 299,
-          address:
-            "108 ซอย โยธา 1 แขวง ตลาดน้อย เขตสัมพันธวงศ์ กรุงเทพมหานคร 10100",
-          lat: "13.7321154",
-          lng: "100.5142296",
-          state: 1,
-        },
-      }
-    },
-  })
-  it("test that bed info edit page have bed amount in input blog.", async () => {
-    await expect(editPage.find("input").element.value).toBe("299")
-  })
-  it("test that bed info edit page have input blog that can change bed amount.", async () => {
-    await expect(editPage.find("input").setValue(555))
-    await expect(editPage.find("input").element.value).toBe("555")
-  })
-  it("test that bed info edit page have submit button for change bed info.", async () => {
-    await expect(editPage.findAll("button").at(1).element.innerHTML).toBe(
-      " แก้ไขที่อยู่ "
     )
   })
 })
@@ -203,67 +70,49 @@ describe("test that findbeds page have all element that must be have", () => {
   })
 })
 
-describe("test that bed reservation page have all elment that must be have.", () => {
-  const bedReserve = mount(Bed, {
-    global: {
-      mocks: {
-        $route: {
-          params: {
-            id: "49",
-          },
-        },
-      },
-    },
-    propsData: {
-      user: mockuser,
-    },
-    date() {
-      return {
-        bed: {
-          address:
-            "100/93 ม 6 irisorchidpark ตำบล บางเมืองใหม่ อำเภอเมืองสมุทรปราการ สมุทรปราการ 10270 ประเทศไทย",
-          amount: 9998,
-          email: "vichayuth2000@hotmail.com",
-          firstname: "วิชยุตม์",
-          id: 49,
-          idcard: "1104700047346",
-          lastname: "ทวิชัยยุทธ",
-          lat: "13.6271161",
-          lineid: "pruktavi",
-          lng: "100.6108495",
-          phone: "0863611590",
-          state: 1,
-          timestamp: "2022-04-09T14:12:11.000Z",
-          user_id: 5,
-        },
-      }
-    },
+describe("test that all Covid - 19 statistic components have all elements that must be have.", () => {
+  const mockDaily = []
+  jest.spyOn(axios, "get").mockResolvedValue(mockDaily)
+  it("test that Chartdaily component have daily graph of Covid - 19 statistic", async () => {
+    const dailyChart = await mount(Chartdaily)
+    await expect(dailyChart.find("canvas").find("#myChart").exists()).toBe(true)
   })
-  it("test that bed reservation page have location map of beds place.", () => {
-    expect(bedReserve.find("div").find("#map").exists()).toBe(true)
-  })
-  it("test that bed reservation page have detail of beds place.", () => {
-    expect(bedReserve.findAll("div").at(2).exists()).toBe(true)
-  })
-  it("test that bed reservation page have input for reservation date.", () => {
-    expect(bedReserve.find("input").element.type).toBe("date")
-  })
-  it("test that bed reservation page have button for reservation date.", () => {
-    expect(bedReserve.find("button").element.innerHTML).toBe(" จอง ")
+  it("test that Chartstats component have all static graph of Covid - 19 statistic", async () => {
+    const allStatChart = await mount(Chartstats)
+    await expect(allStatChart.find("#myChart").exists()).toBe(true)
+    await expect(allStatChart.find("#myChart2").exists()).toBe(true)
+    await expect(allStatChart.find("#myChart3").exists()).toBe(true)
   })
 })
 
-describe("test that add bed page have all element that must be have.", () => {
-  it("test that add bed page have input for bed amount.", () => {
-    expect(true).toBe(true)
-  })
-  it("test that add bed page have detail blog of user info.", () => {
-    expect(true).toBe(true)
-  })
-  it("test that add bed page have input for select location of beds place.", () => {
-    expect(true).toBe(true)
-  })
-  it("test that add bed page have submit button for add beds place.", () => {
-    expect(true).toBe(true)
+describe("test that Beds Manage page have all elements that must be have.", () => {
+  const myBeds = [
+    {
+      address:
+        "108 ซอย โยธา 1 แขวง ตลาดน้อย เขตสัมพันธวงศ์ กรุงเทพมหานคร 10100",
+      amount: 299,
+      customer_amount: 1,
+      id: 50,
+      lat: "13.7321154",
+      lng: "100.5142296",
+      state: "true",
+      timestamp: "2022-04-10T08:14:12.000Z",
+    },
+  ]
+  it("test that Beds Manage page have details blogs, edit bed page link button and add bed button.", async () => {
+    const bedsManage = await mount(Bedsmanage, {
+      data() {
+        return {
+          beds: myBeds,
+        }
+      },
+    })
+    await expect(bedsManage.find("table").exists()).toBe(true)
+    await expect(bedsManage.findAll("button").at(2).element.innerHTML).toBe(
+      "แก้ไข"
+    )
+    await expect(bedsManage.findAll("button").at(0).element.innerHTML).toBe(
+      "เพิ่มสถานที่"
+    )
   })
 })
